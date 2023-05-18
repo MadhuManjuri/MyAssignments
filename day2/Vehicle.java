1c8f4218772f196850876c5a7bba8b8dc9d78693
MadhuManjuri <133889420+MadhuManjuri@users.noreply.github.com>
2023-05-18 10:47:05 +0530

    Add files via upload
    Day 2 classroom activity

---
+++ day2/Vehicle.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class Vehicle {
+
+	public static void main(String[] args) {
+		int no = 5;
+		boolean petrol = true;
+		String model = "Audi";
+		//f or F
+	float mileage = 23.45f;
+	
+	//l or L
+	long insuranceno = 234252534555555L;
+	
+	char logo = 'H';
+	
+		System.out.println("Vehicle no:"+no);
+		System.out.println("Type of fuel pertrol : "+petrol);
+		System.out.println("Model type : "+model);
+		System.out.println("Mileage : "+mileage);
+		System.out.println("InsuranceNo "+insuranceno);
+		System.out.println("Logo : "+logo);
+	}
+
+}
